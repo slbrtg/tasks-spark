ca0bf337c9295f37690818ca4041cc8584506a31
Saul Artiga <slbrtg@gmail.com>
2017-07-03 16:39:57 -0700

    complete retrieving list of all objects

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,33 @@
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Task {
   private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private static List<Task> instances = new ArrayList<Task>();
 
   public Task (String description){
     mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
   }
 
   public String getDescription(){
     return mDescription;
+  }
+
+  public boolean isCompleted(){
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt(){
+    return mCreatedAt;
+  }
+
+  public static List<Task> all(){
+    return instances;
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -13,5 +14,25 @@ public class TaskTest {
   public void Task_instantiatesWithDescription_String(){
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false(){
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedtAt_instaniatesWithCurrentTime_today(){
+    Task myTask = new Task("mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTask_true(){
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(true, Task.all().contains(firstTask));
+    assertEquals(true, Task.all().contains(secondTask));    
   }
 }
